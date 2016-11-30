a55077e3fde39d93a1bf8611c61cac0135e86579
Victor Mwenda <vmwenda.vm@gmail.com>
2016-11-30 15:13:55 +0300

    Created the app landing page

--- views/site/index.php
+++ views/site/index.php
@@ -7,5 +7,64 @@ use app\database\DbWorker;
 $this->title = 'My Yii Application';
 ?>
 <div class="site-index">
-    <?php print_r((new DbWorker())->getClassesArray()); ?>
+    <div class="row">
+        <div class="col-md-6">
+            <div class="panel-primary">
+                <div class="panel-heading">
+                    <label>School</label>
+                </div>
+                <div class="panel-body">
+                    <label>In the schools module, you can create as many schools as you can. This makes the application
+                        extensible</label>
+                </div>
+                <div class="panel-footer">
+                    <a class="btn btn-outline btn-primary" href="index.php?r=schools">Go to</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-6">
+            <div class="panel-primary">
+                <div class="panel-heading">
+                    <label>Classes</label>
+                </div>
+                <div class="panel-body">
+                    <label>In the classes model, we create classes which belong to schools</label>
+                </div>
+                <div class="panel-footer">
+                    <a class="btn btn-outline btn-primary" href="index.php?r=classes">Go to</a>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div style="margin: 20px;"></div>
+    
+    <div class="row">
+        <div class="col-md-6">
+            <div class="panel-primary">
+                <div class="panel-heading">
+                    <label>Students</label>
+                </div>
+                <div class="panel-body">
+                    <label>In the students module, we create students who exist in classes</label>
+                </div>
+                <div class="panel-footer">
+                    <a class="btn btn-outline btn-primary" href="index.php?r=students">Go to</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-6">
+            <div class="panel-primary">
+                <div class="panel-heading">
+                    <label>Class Prefects</label>
+                </div>
+                <div class="panel-body">
+                    <label>In the class prefects module, we create students who have the roles of being a class
+                        prefect</label>
+                </div>
+                <div class="panel-footer">
+                    <a class="btn btn-outline btn-primary" href="index.php?r=class-prefects">Go to</a>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
