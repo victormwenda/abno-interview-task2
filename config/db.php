731d873c314f08eda345047d6ad37613951f2561
Victor Mwenda <vmwenda.vm@gmail.com>
2016-11-30 13:21:05 +0300

    Added the right database conection

--- config/db.php
+++ config/db.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
+    'dsn' => 'mysql:host=localhost;dbname=sch-mgmt-sys',
     'username' => 'root',
     'password' => '',
     'charset' => 'utf8',
